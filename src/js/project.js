1f19422e8fdcf01c171eef53c1c14393654c6eb7
isushkov <sushkov.ilia@gmail.com>
2024-02-17 20:21:31 -0500

    project page - stable

--- src/js/project.js
+++ src/js/project.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
+  // updImage 
+  var src = $("#thumbnail img").first().attr("src");
+  var match = src.match(/\.\/img\/projects\/([^\/]+)\/[^\/]+$/);
+  var page = match ? match[1] : 'defaultPage';
+  var dataImgOthers = $("#dataImgOthers").text().split(';');
+  $.each(dataImgOthers, function(index, img) {
+    var listItem = $("<li>").html(`<img onclick="changeImage(this)" src="./img/projects/${page}/${img}" width="70">`);
+    $("#thumbnail").append(listItem);
+  });
+  // changeImage
+  $('#thumbnail img').click(function() {
+    var newImage = $(this).attr('src');
+    $('#mainProductImage').attr('src', newImage);
+  });
   // scopeOfWork
   var tasks = $('.scope-of-work-item').text().split(';');
   $('.scope-of-work-item').remove();
   $.each(tasks, function(i, task) {
     task = $.trim(task);
     if (task) {
-      $('.scope-of-work').append('<li class="scope-of-work-item">' + task + '</li>');
+      $('.scope-of-work').append('<li class="scope-of-work-item"><p><i class="fas fa-circle"></i>' + task + '</p></li>');
     }
-  });
-  // changeImage
-  $('#thumbnail img').click(function() {
-    var newImage = $(this).attr('src');
-    $('#main_product_image').attr('src', newImage);
   });
 });
--- src/js/projects-carousel.js
+++ src/js/projects-carousel.js
@@ -2,16 +2,12 @@ $(document).ready(function() {
   const $data = $('#projectCarouselData').children();
   const $carouselInner = $('#projectCarousel .carousel-inner').empty();
   let currentSlide;
-
-  // Функция для создания нового слайда
   function createNewSlide() {
-    const $slide = $('<div class="carousel-item"><div class="d-flex justify-content-center"></div></div>');
+    const $slide = $('<div class="carousel-item"><div class="carousel-item-wrapper"></div></div>');
     if ($carouselInner.children().length === 0) $slide.addClass('active'); // Делаем первый слайд активным
     $carouselInner.append($slide);
-    return $slide.find('.d-flex');
+    return $slide.find('.carousel-item-wrapper');
   }
-
-  // Функция добавления карточек в слайды
   function addCardsToSlides() {
     let cardsPerSlide;
     if ($(window).width() > 768) {
@@ -19,7 +15,6 @@ $(document).ready(function() {
     } else {
       cardsPerSlide = 2;
     }
-
     currentSlide = createNewSlide();
     $data.each(function(index) {
       if (index % cardsPerSlide === 0 && index !== 0) {
@@ -28,11 +23,7 @@ $(document).ready(function() {
       currentSlide.append($(this));
     });
   }
-
-  // Инициализация
   addCardsToSlides();
-
-  // Обновление при изменении размера окна
   $(window).resize(function() {
     $carouselInner.empty(); // Очищаем карусель
     addCardsToSlides(); // Пересоздаем слайды
