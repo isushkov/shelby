0668157a3b6d0cd9438344c490513748309ba419
isushkov <sushkov.ilia@gmail.com>
2024-02-17 23:27:46 -0500

    project page - init design

--- src/js/project.js
+++ src/js/project.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
-  // updImage 
-  var src = $("#thumbnail img").first().attr("src");
+  // updImage
+  var src = $("#thumbnails img").first().attr("src");
   var match = src.match(/\.\/img\/projects\/([^\/]+)\/[^\/]+$/);
   var page = match ? match[1] : 'defaultPage';
   var dataImgOthers = $("#dataImgOthers").text().split(';');
   $.each(dataImgOthers, function(index, img) {
-    var listItem = $("<li>").html(`<img onclick="changeImage(this)" src="./img/projects/${page}/${img}" width="70">`);
-    $("#thumbnail").append(listItem);
+    var listItem = $('<li class="thumbnail">').html(`<img class='thumbnail-image' onclick='changeImage(this)' src='./img/projects/${page}/${img}'>`);
+    $("#thumbnails").append(listItem);
   });
   // changeImage
-  $('#thumbnail img').click(function() {
+  $('#thumbnails img').click(function() {
     var newImage = $(this).attr('src');
-    $('#mainProductImage').attr('src', newImage);
+    $('.fade-overlay').fadeIn(300, function() {
+      $('#mainProjectImage').attr('src', newImage);
+      $('.main-image .blur-overlay').attr('src', newImage);
+      $('.fade-overlay').fadeOut(300);
+    });
   });
   // scopeOfWork
   var tasks = $('.scope-of-work-item').text().split(';');
