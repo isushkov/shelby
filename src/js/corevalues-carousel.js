1d05b402a1ef4c2d94d349bde6df2be09205bdb4
isushkov <sushkov.ilia@gmail.com>
2024-02-24 17:00:31 -0500

    about us - paste content done

---
+++ src/js/corevalues-carousel.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  const $data = $('#coreValuesData').children();
+  const $carouselInner = $('#coreValuesCarousel .carousel-inner').empty();
+  let currentSlide;
+  function createNewSlide() {
+    const $slide = $('<div class="carousel-item corevalues-item"><div class="carousel-item-wrapper corevalues-item-wrapper"></div></div>');
+    if ($carouselInner.children().length === 0) $slide.addClass('active'); // Делаем первый слайд активным
+    $carouselInner.append($slide);
+    return $slide.find('.corevalues-item-wrapper');
+  }
+  function addCardsToSlides() {
+    let cardsPerSlide;
+    if ($(window).width() > 991) {
+      cardsPerSlide = 3;
+    } else if ($(window).width() > 550) {
+      cardsPerSlide = 2;
+    } else {
+      cardsPerSlide = 1;
+    }
+    currentSlide = createNewSlide();
+    $data.each(function(index) {
+      if (index % cardsPerSlide === 0 && index !== 0) {
+        currentSlide = createNewSlide();
+      }
+      currentSlide.append($(this));
+    });
+  }
+  addCardsToSlides();
+  $(window).resize(function() {
+    $carouselInner.empty(); // Очищаем карусель
+    addCardsToSlides(); // Пересоздаем слайды
+  });
+});
